96559e71900c342f46fe6a05c92b884374555dbc
kamkara <lnclass.kamkara@gmail.com>
2022-07-05 12:40:29 +0000

    app/controllers/application_controller: add user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+
+    # Add sign up params
+    protect_from_forgery with: :exception
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+    #After sign in
+    def after_sign_in_path_for(resource)
+        root_path
+    end
+    
+    def after_sign_up_path_for(resource)
+        root_path
+    end
+
+    # If you have extra params to permit, append them to the sanitizer.
+    def configure_permitted_parameters
+      #sign_up_params = [ :first_name, :last_name, :full_name, :matricule, :level_name,
+      #:material_name, :school_name, :contact, :city_name, :status, :email,
+    #                 :password, :slug, :gender, :payment, :terms, :avatar_profil, :referral_in, :referral_out]
+                                
+    #update_params  = [ :school_name, :status, :avatar_profil, :status_payment]
+     
+    sign_up_params = [ :first_name, :last_name, :full_name, :contact, :status, :email, :password,
+                        :city_name, :school_name, :level_name, :matricule, :gender, :slug]
+      devise_parameter_sanitizer.permit :sign_up, keys: sign_up_params
+      devise_parameter_sanitizer.permit :account_update, keys: sign_up_params
+      devise_parameter_sanitizer.permit :sign_in, keys: [:logged, :password]
+    end 
 end
