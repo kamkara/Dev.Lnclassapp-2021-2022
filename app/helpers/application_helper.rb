da12f291af748a7f98e253502d95e9a51a07a2c1
kamkara <lnclass.kamkara@gmail.com>
2022-07-05 11:06:44 +0000

    Add custom ongle message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    #Custom Onglet Title
+    def title
+      base_title = "Lnclass Education"
+      if @title.nil?
+        base_title
+      else
+        "#{@title} | #{base_title}"
+      end
+    end
+    #site description
+    def description
+        "Lnclass: facilité les études"
+    end
 end
